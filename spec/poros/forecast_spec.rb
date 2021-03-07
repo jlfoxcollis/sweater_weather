cc88471f281eb67b62c14e4bb60aab70966a96cd
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-07 15:14:53 -0700

    tested forecast poro

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -2,22 +2,17 @@ require 'rails_helper'
 
 describe Forecast do
   it 'exists' do
-    data = {
-      dt: 1615316400,
-      sunrise: 1615296030,
-      sunset: 1615338029,
-      temp: {
-          day: 288.44,
-          min: 279.6,
-          max: 290.56,
-          night: 282.9,
-          eve: 287.56,
-          morn: 279.6
-      }
-    }
+    data = File.read("spec/fixtures/open_weather_denver.json")
+    parsed = JSON.parse(data, symbolize_names: true)
+    result = Forecast.new(parsed)
 
-    result = Forecast.new(data)
+    expect(result).to be_a(Forecast)
+    expect(result.current_weather.sunrise).to eq("Sun,Mar 07 2021 06:23:38 -0700")
+    expect(result.current_weather).to be_a(CurrentForecast)
+    expect(result.daily_weather).to be_an(Array)
+    expect(result.daily_weather.first).to be_a(DailyForecast)
+    expect(result.hourly_weather).to be_an(Array)
+    expect(result.hourly_weather.first).to be_an(HourlyForecast)
 
-    expect(result.current_weather.sunrise).to eq("March")
   end
 end
