3652b39a165a11a64495340acfa8a213e4ca3ae3
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-07 15:45:40 -0700

    tested services

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -6,7 +6,11 @@ describe MapquestGeoApi do
     stub_request(:get, /address/).to_return(
       status: 200, body: denver
       )
+    params = Hash.new
+    params[:location] = "Denver,CO"
+    results = MapquestGeoApi.get_location(params)
 
-      
+    expect(results[:results].first[:locations].first[:latLng]).to have_key(:lat)
+    expect(results[:results].first[:locations].first[:latLng]).to have_key(:lng)
   end
 end
--- spec/services/open_weather_api_spec.rb
+++ spec/services/open_weather_api_spec.rb
@@ -2,6 +2,20 @@ require 'rails_helper'
 
 describe OpenWeatherApi do
   it 'can get one time weather information for location' do
+    denver = File.read('spec/fixtures/open_weather_denver.json')
+    stub_request(:get, /onecall/).to_return(
+      status: 200, body: denver
+      )
 
+    params = Hash.new
+    params[:lat] = 39.738453
+    params[:lng] = -104.984853
+    results = OpenWeatherApi.get_location(params)
+
+    expect(results).to have_key(:current)
+    expect(results).to have_key(:daily)
+    expect(results).to have_key(:hourly)
+    expect(results).to_not have_key(:minutely)
+    expect(results).to_not have_key(:alerts)
   end
 end
