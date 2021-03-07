672b658150808825d4e584dc17f7783704c223fb
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-07 14:56:39 -0700

    tested curent weather poro

--- spec/poros/current_forecast_spec.rb
+++ spec/poros/current_forecast_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Forecast do
   it 'exists' do
-    data = "dt": 1615147496,
+    data = {"dt": 1615147496,
     "sunrise": 1615123418,
     "sunset": 1615165104,
     "temp": 292.24,
@@ -28,6 +28,15 @@ describe Forecast do
 
     result = CurrentForecast.new(data)
 
-    expect(result.current_weather.sunrise).to eq("March")
+    expect(result.sunrise).to eq("Sun,Mar 07 2021 06:23:38 -0700")
+    expect(result.datetime).to eq("Sun,Mar 07 2021 13:04:56 -0700")
+    expect(result.sunset).to eq("Sun,Mar 07 2021 17:58:24 -0700")
+    expect(result.temperature).to eq(292.24)
+    expect(result.feels_like).to eq(288.34)
+    expect(result.humidity).to eq(10)
+    expect(result.uvi).to eq(4.13)
+    expect(result.visibility).to eq(10000)
+    expect(result.conditions).to eq("overcast clouds")
+    expect(result.icon).to eq("04d")
   end
 end
