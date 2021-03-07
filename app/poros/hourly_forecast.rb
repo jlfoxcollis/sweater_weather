e5520b27745066bbbb8408a8f1639f3eaa822fad
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-07 15:08:05 -0700

    tested hourly forecast poro

--- app/poros/hourly_forecast.rb
+++ app/poros/hourly_forecast.rb
@@ -1,16 +1,17 @@
 class HourlyForecast
   attr_reader :temperature,
               :conditions,
-              :icon
+              :icon,
+              :time
 
   def initialize(data)
-    @time = time(Time.at(data[:dt]).to_datetime)
+    @time = datetime(Time.at(data[:dt]).to_datetime)
     @temperature = data[:temp]
     @conditions = data[:weather].first[:description]
     @icon = data[:weather].first[:icon]
   end
 
-  def time(data)
+  def datetime(data)
     data.strftime("%I:%M:%S %p")
   end
 end
--- spec/poros/hourly_forecast_spec.rb
+++ spec/poros/hourly_forecast_spec.rb
@@ -1,23 +1,36 @@
 require 'rails_helper'
 
-describe Forecast do
+describe HourlyForecast do
   it 'exists' do
+
     data = {
-      dt: 1615316400,
-      sunrise: 1615296030,
-      sunset: 1615338029,
-      temp: {
-          day: 288.44,
-          min: 279.6,
-          max: 290.56,
-          night: 282.9,
-          eve: 287.56,
-          morn: 279.6
-      }
+      dt: 1615309200,
+      temp: 285.95,
+      feels_like: 283.18,
+      pressure: 1010,
+      "humidity": 34,
+      "dew_point": 264.31,
+      "uvi": 3.7,
+      "clouds": 88,
+      "visibility": 10000,
+      "wind_speed": 0.61,
+      "wind_deg": 345,
+      "weather": [
+          {
+              "id": 804,
+              "main": "Clouds",
+              "description": "overcast clouds",
+              "icon": "04d"
+          }
+      ],
+      "pop": 0
     }
 
     result = HourlyForecast.new(data)
+    expect(result.time).to eq("10:00:00 AM")
+    expect(result.temperature).to eq(285.95)
+    expect(result.conditions).to eq("overcast clouds")
+    expect(result.icon).to eq("04d")
 
-    expect(result.current_weather.sunrise).to eq("March")
   end
 end
