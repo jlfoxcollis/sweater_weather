b470fdcff1418781ed76e0dfec698680868562b7
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-07 17:03:37 -0700

    tested forecast Facade for background

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -20,7 +20,25 @@ describe ForecastFacade do
       results = ForecastFacade.map_quest(params)
       expect(results).to have_attributes(:current_weather => CurrentForecast, :daily_weather => Array, :hourly_weather => Array)
       expect(results.daily_weather.count).to eq(5)
-      expect(results.hourly_weather.count).to eq(8)
-      
+      expect(results.hourly_weather.count).to eq(8)   
+  end
+
+  it 'can get a background image' do
+    unsplash = File.read('spec/fixtures/unsplash_response.json')
+    stub_request(:get, /photos/).to_return(
+      status: 200, body: unsplash
+      )
+    params = Hash.new
+    params[:location] = "Denver,CO"
+    actual = ForecastFacade.get_background(params)
+    expect(actual).to be_a(Background)
+    expect(actual).to have_attributes(:image => Hash)
+    expect(actual.image).to have_key(:description)
+    expect(actual.image[:description]).to be_a(String)
+    expect(actual.image).to have_key(:location)
+    expect(actual.image).to have_key(:image_url)
+    expect(actual.image).to have_key(:credit)
+    expect(actual.image[:credit]).to have_key(:source)
+    expect(actual.image[:credit]).to have_key(:author)
   end
 end
