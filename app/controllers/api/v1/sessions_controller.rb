a3c45f75744b90c2bb44142e0160d46b5200814c
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-08 07:21:03 -0700

    sad path testing for sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,9 +2,13 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+    begin
     if user.authenticate(params[:password])
       render json: UserSerializer.new(user)
     else
+      render json: {"error": "Invalid Credentials"}
+    end
+    rescue
       render json: {"error": "Invalid Credentials"}
     end
   end
--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'when I receive a post request for sessions' do
   it 'can start a new session' do
-    user = User.create(email: "admin@example.com", password: "trigun", password: "trigun")
+    user = User.create(email: "admin@example.com", password: "trigun", password_confirmation: "trigun")
     headers = {
       'Accept' => 'application/json', 
       'Content-Type' => 'application/json'
@@ -24,4 +24,69 @@ describe 'when I receive a post request for sessions' do
     expect(parsed[:data][:attributes]).to_not have_key(:password)
     expect(parsed[:data][:attributes]).to_not have_key(:password_confirmation)
   end
+
+  it 'can fail to start new session' do
+    user = User.create(email: "admin@example.com", password: "trigun", password_confirmation: "trigun")
+    headers = {
+      'Accept' => 'application/json', 
+      'Content-Type' => 'application/json'
+    }
+    post '/api/v1/sessions', params: JSON.generate(email: "admin@example.com", password: "noob"), headers: headers
+
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq("Invalid Credentials")
+  end
+
+  it 'can fail to start new session' do
+    user = User.create(email: "admin@example.com", password: "trigun", password_confirmation: "trigun")
+    headers = {
+      'Accept' => 'application/json', 
+      'Content-Type' => 'application/json'
+    }
+    post '/api/v1/sessions', params: JSON.generate(email: "admin@example.com", password: ""), headers: headers
+
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq("Invalid Credentials")
+  end
+
+  it 'can fail to start new session' do
+    user = User.create(email: "admin@example.com", password: "trigun", password_confirmation: "trigun")
+    headers = {
+      'Accept' => 'application/json', 
+      'Content-Type' => 'application/json'
+    }
+    post '/api/v1/sessions', params: JSON.generate(email: "adm1n@example.com", password: "noob"), headers: headers
+
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq("Invalid Credentials")
+  end
+
+  it 'can fail to start new session' do
+    user = User.create(email: "admin@example.com", password: "trigun", password_confirmation: "trigun")
+    headers = {
+      'Accept' => 'application/json', 
+      'Content-Type' => 'application/json'
+    }
+    post '/api/v1/sessions', params: JSON.generate(email: "admin@example.com", password: "noob"), headers: headers
+
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:error)
+    expect(parsed[:error]).to eq("Invalid Credentials")
+  end
+
 end
