03bbcc521078df38bcf1f17e7891973bf87ff44a
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-09 22:09:17 -0700

    testing 100%

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -40,6 +40,40 @@ describe 'when I want to go on a trip' do
   end
 
   it 'can return the roadtrip details' do
+    denver = File.read('spec/fixtures/lhctoanchorage.json')
+    stub_request(:get, /route/).to_return(
+      status: 200, body: denver, headers: {}
+      )
+
+    denver_forecast = File.read('spec/fixtures/open_weather_denver.json')
+    stub_request(:get, /onecall/).to_return(
+      status: 200, body: denver_forecast, headers: {}
+      )
+    headers = {
+      'Accept' => 'application/json', 
+      'Content-Type' => 'application/json'
+    }
+    post "/api/v1/road_trip", params: JSON.generate("road_trip": {"origin": "LakeHavasuCity,AZ", "destination": "Anchorage,AK", "api_key": @user.api_key}), headers: headers
+
+    expect(response).to be_successful
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:data)
+    expect(parsed[:data]).to have_key(:id)
+    expect(parsed[:data]).to have_key(:type)
+    expect(parsed[:data]).to have_key(:attributes)
+    trip = parsed[:data][:attributes]
+
+    expect(trip).to have_key(:start_city)
+    expect(trip).to have_key(:end_city)
+    expect(trip).to have_key(:travel_time)
+    expect(trip).to have_key(:weather_at_eta)
+    expect(trip[:weather_at_eta]).to have_key(:temperature)
+    expect(trip[:weather_at_eta]).to have_key(:conditions)
+  end
+
+  it 'can return the roadtrip details' do
     anchorage = File.read('spec/fixtures/anchorage.json')
     stub_request(:get, /route/).to_return(
       status: 200, body: anchorage, headers: {}
