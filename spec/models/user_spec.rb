c4445a96febeb9d9853a6096ea7430ca4f5e6a6f
James Fox-Collis <jlfoxcollis@gmail.com>
2021-03-09 22:32:31 -0700

    user validations for secure password and token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,7 @@ describe User do
   describe 'validations' do
     it { should validate_presence_of :password }
     it { should validate_uniqueness_of :email }
+    it { should have_secure_password }
+    it { should have_secure_token :api_key }
   end
 end
